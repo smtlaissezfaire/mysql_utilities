70990ed1b3d7af8b01c4e519634fd0b7cf6071d8
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2007-11-25 10:23:42 +0000

    better design of Object.lazy_attr_reader
    git-svn-id: http://thmadb.com/public_svn/plugins/mysql_utilities@100 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- lib/mysql_utilities/extensions/object.rb
+++ lib/mysql_utilities/extensions/object.rb
@@ -1,40 +1,39 @@
 module MysqlUtilities
-  module ObjectExtensions
-    module ClassMethods
-      
-      # A call like the following: 
-      #
-      # lazy_attr_reader(:mysql_user)     { database["username"] }      
-      #
-      # Would add an instance method like the following:
-      #
-      # def mysql_user
-      #   @mysql_user ||= database["username"]
-      # end
-      #      
-      # The block given to lazy_attr_reader will be a default value.  
-      # the block is given so that it evaluates at run time, and in
-      # instance's context.
-      #
-      # If you don't care about that sort of thing, you can just
-      # give it a second argument (but then it will evaluate on the class level)
-      def lazy_attr_reader(instance_variable_name, default_value=nil, &default_value_blk)
-        block_given = default_value_blk ? true : false
-        
-        define_method(instance_variable_name) do
-          value = instance_variable_get("@#{instance_variable_name}")
-          value ||= block_given ? instance_eval(&default_value_blk) : default_value
-          instance_variable_set("@#{instance_variable_name}", value)
-          value
-        end          
-      end
-    end      
+  module Extensions
+    module Object
+      module ClassMethods
+
+        # A call like the following: 
+        #
+        # lazy_attr_reader(:mysql_user)     { database["username"] }      
+        #
+        # Would add an instance method like the following:
+        #
+        # def mysql_user
+        #   @mysql_user ||= database["username"]
+        # end
+        #      
+        # The block given to lazy_attr_reader will be a default value.  
+        # the block is given so that it evaluates at run time, and in
+        # instance's context.
+        #
+        # If you don't care about that sort of thing, you can just
+        # give it a second argument (but then it will evaluate on the class level)
+        def lazy_attr_reader(instance_variable_name, default_value=nil, &default_block)
+          raise ArgumentError, "lazy_attr_reader cannot take two default values" if default_block && default_value
+          define_method(instance_variable_name) do
+            value = instance_variable_get("@#{instance_variable_name}")
+            value ||= default_block ? instance_eval(&default_block) : default_value
+          end
+        end
+      end      
+    end
   end
 end
 
 class Object  
   class << self
-    include DatabaseUtilities::ObjectExtensions::ClassMethods
+    include MysqlUtilities::Extensions::Object::ClassMethods
   end
 end
 
---
+++ spec/extensions/object_spec.rb
@@ -0,0 +1,49 @@
+require File.dirname(__FILE__) + "/spec_helper"
+
+describe Object do
+  before :each do
+    @klass = Class.new
+  end
+  
+  it "should respond to lazy_attr_reader" do
+    @klass.respond_to?(:lazy_attr_reader).should be_true
+  end
+  
+  it "should take a first argument - the name of the method" do
+    @klass.lazy_attr_reader(:foo)
+  end
+  
+  it "should take an optional second argument - the default value of the method" do
+    @klass.lazy_attr_reader(:foo, :bar)
+  end
+  
+  it "should add an attr_reader with a nil value, when no default value is given" do
+    @klass.lazy_attr_reader(:foo)
+    @klass.new.foo.should be_nil
+  end
+  
+  it "should add an attr_reader, with a default value, if specified" do
+    @klass.lazy_attr_reader(:foo, :bar)
+    @klass.new.foo.should == :bar
+  end
+  
+  it "should be able to change the default value, by changing the instance variable" do
+    @klass.lazy_attr_reader(:foo, :bar)
+    instance = @klass.new
+    instance.instance_variable_set("@foo", 7)
+    instance.foo.should == 7    
+  end
+
+  it "should be able to take a block as it's default parameter" do
+    @klass.lazy_attr_reader(:foo) { 1 }
+    @klass.new.foo.should == 1
+  end
+  
+  it "should raise an error if both are specified" do
+    lambda { 
+      @klass.lazy_attr_reader(:foo, :bar) { :baz }
+    }.should raise_error(ArgumentError, "lazy_attr_reader cannot take two default values")
+    
+  end
+
+end
