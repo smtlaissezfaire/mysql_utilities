923a8925120fdc03ded5a9275bb8bdf958a5e504
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2007-11-25 10:38:38 +0000

    redesigning Backup::Version
    git-svn-id: http://thmadb.com/public_svn/plugins/mysql_utilities@105 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- lib/mysql_utilities/backup.rb
+++ lib/mysql_utilities/backup.rb
@@ -7,6 +7,8 @@ require 'yaml'
 require File.dirname(__FILE__) + "/extensions"
 require File.dirname(__FILE__) + "/backup/core_functionality"
 require File.dirname(__FILE__) + "/backup/option_parser"
+require File.dirname(__FILE__) + "/backup/version"
+
 
 # TODO: This should be done in the command line tool:
 # MysqlUtilities::Backup::OptionParser.parse
--- lib/mysql_utilities/backup/version.rb
+++ lib/mysql_utilities/backup/version.rb
@@ -1,10 +1,14 @@
 module MysqlUtilities
   module Backup
     module Version
-      class << self
-        def to_s
-          "0.0.1"
-        end
+      MAJOR = 0
+      MINOR = 0
+      TINY = 1
+      
+    module_function
+      
+      def to_s
+        "#{MAJOR}.#{MINOR}.#{TINY}"
       end
     end
   end
